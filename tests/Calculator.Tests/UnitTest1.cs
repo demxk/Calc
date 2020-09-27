b222c85174d5003336a08f4b2f4a63f166848f0b
demxk <damirmusik@mail.ru>
2020-09-28 00:14:53 +0300

    added csharp calc

---
+++ tests/Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+using Calculator;
+
+namespace Calculator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(3, Calculator.Add(1, 2));
+            Assert.Equal(1, Calculator.Add(-1, 2));
+            Assert.Equal(4, Calculator.Sub(3, -1));
+            Assert.Equal(2, Calculator.Mul(1, 2));
+            Assert.Equal(555, Calculator.Mul(5, 111));
+            Assert.Equal(6, Calculator.Div(19, 3));
+            Assert.ThrowsAny<Exception>(() => Calculator.Div(1, 0));
+        }
+
+        /*
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1, Calculator.Add(1, 2));
+        }
+        */
+    }
+}
