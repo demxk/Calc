66c6b215673d8146b0c6df97c1e3e4500c1264a5
demxk <damirmusik@mail.ru>
2020-09-27 20:13:20 +0300

    first

---
+++ src/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Calculator
+    {
+        public static int Add(int a, int b) => a + b;
+        public static int Sub(int a, int b) => a - b;
+        public static int Mul(int a, int b) => a * b;
+
+        public static int Div(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
