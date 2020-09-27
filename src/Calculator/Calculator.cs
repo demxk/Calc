4713fb33405e3ec4310258a2d487e142c9234eea
demxk <damirmusik@mail.ru>
2020-09-27 23:58:11 +0300

    csharp cacl

--- src/Calculator/Calculator.cs
+++ src/Calculator/Calculator.cs
@@ -2,19 +2,11 @@
 
 namespace Calculator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-
     public class Calculator
     {
         public static int Add(int a, int b) => a + b;
         public static int Sub(int a, int b) => a - b;
         public static int Mul(int a, int b) => a * b;
-
         public static int Div(int a, int b)
         {
             if (b == 0)
