634cf55b61239ca27ac0fb84a97aebe1d756eeb9
demxk <damirmusik@mail.ru>
2020-09-28 16:37:20 +0300

    fsharp calc

---
+++ src/App/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Library
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    calc 1 0 Div |> print
+    0 // return an integer exit code
---
+++ src/FLib/Library.fs
@@ -0,0 +1,21 @@
+﻿module Library
+
+open System
+
+type Op =
+| Add
+| Sub
+| Mul
+| Div
+
+let calc a b op = 
+    match op with
+    | Add -> Ok (a + b)
+    | Sub -> Ok (a - b)
+    | Mul -> Ok (a * b)
+    | _  -> if b <> 0 then Ok (a / b) else Error DivideByZeroException
+
+let print res =
+    match res with
+    | Ok value -> printf "Ok, value: %i" value
+    | Error _ -> printf "Error, div by zero"
